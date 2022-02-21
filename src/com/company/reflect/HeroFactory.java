2d51afd7f69a8d4218e403c480be68357d11a3dc
zhengsongjun <zheng_songjun@venusgroup.com.cn>
2022-02-21 15:26:17 +0800

    feat: 补全类名

--- src/com/company/reflect/HeroFactory.java
+++ src/com/company/reflect/HeroFactory.java
@@ -6,7 +6,8 @@ public class HeroFactory {
     public static SkillImpl getHero (String name) throws Exception {
         SkillImpl iSkill;
         // 元类  类是对象的抽象，元类是描述一个类
-        Class<?> cla = Class.forName(name);
+        String classString = "reflect.hreo." + name;
+        Class<?> cla = Class.forName(classString);
         Object obj = cla.newInstance();
         return (SkillImpl)obj;
     };
